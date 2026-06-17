c703c1ac80b3b1a1b3c2e03c95484ca9fc711b8a
imixiu <174745401@qq.com>
2026-06-17 20:14:49 +0800

    fix: add Google Analytics (G-95PY8PSZ0Y) to all pages
    - Add GA script to layout.tsx <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-95PY8PSZ0Y"></script>
+        <script dangerouslySetInnerHTML={{ __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-95PY8PSZ0Y');" }} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(webSiteJsonLd) }}
